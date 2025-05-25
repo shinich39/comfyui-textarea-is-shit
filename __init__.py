da2c9a326efe0a857d9f112d8a7b92efd95660e2
shinich39 <shinich39@gmail.com>
2025-05-25 19:15:23 +0900

    Updated on 05/25/2025

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 @author: shinich39
 @title: comfyui-textarea-is-shit
 @nickname: comfyui-textarea-is-shit
-@version: 1.0.8
+@version: 1.0.9
 @description: HTML gives me a textarea like piece of shit.
 """
 
